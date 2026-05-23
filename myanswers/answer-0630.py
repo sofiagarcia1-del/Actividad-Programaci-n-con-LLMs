17617278dd6ff5425fc8f292c8cc8eb361da7fce
sofiagarcia1-del <sofia.garcia1@udea.edu.co>
2026-05-23 11:07:51 -0500

    Add function to detect out-of-bounds measurements

--- myanswers/answer-0630.py
+++ myanswers/answer-0630.py
@@ -1,1 +1,8 @@
-
+def detectar_fuera_de_limites(df_medidas, lcl, ucl):
+    fuera = df_medidas[
+        (df_medidas["medida_mm"] < lcl) | (df_medidas["medida_mm"] > ucl)
+    ]["id_lote"].tolist()
+    return {
+        "total_lotes": len(df_medidas),
+        "fuera_de_control": fuera
+    }
